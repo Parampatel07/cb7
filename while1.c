36e1159d89b0cdf3aa70374e63d37a209b7f2819
Parampatel07 <90458806+Parampatel07@users.noreply.github.com>
2022-07-20 20:27:29 +0530

    loop

---
+++ while1.c
@@ -0,0 +1,11 @@
+// write a programe to print your name 100 times
+#include <stdio.h>
+void main()
+{
+    int count = 0;
+    while(1)
+    {
+        printf("name ");
+        count = count + 1;
+    }
+}
