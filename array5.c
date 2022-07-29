9bb8e66fa236d2ac52b7ff92a5873b9430d930e3
Parampatel07 <90458806+Parampatel07@users.noreply.github.com>
2022-07-29 20:31:36 +0530

    array

--- array5.c
+++ array5.c
@@ -2,14 +2,28 @@
 #include <stdio.h>
 void main()
 {
-    int numbers[10], count = 0;
+    int numbers[10], count = 0, temp, flash;
     for (count = 0; count < 10; count++)
     {
         printf("Enter number %d  ", count + 1);
         scanf("%d", &numbers[count]);
-    }
-    for(count=0;count<10;count++)
+    } // loop for input
+
+    for (flash = 0; flash < 9; flash++)
     {
-        printf("\nnumber %d is %d", count+1,numbers[count]);
+        for (count = flash; count < 10; count++)
+        {
+            if (numbers[flash] > numbers[count])
+            {
+                temp = numbers[flash];
+                numbers[flash] = numbers[count];
+                numbers[count] = temp;
+            }
+        }
+    }
+
+    for (count = 0; count < 10; count++)
+    {
+        printf("\nnumber %d is %d", count + 1, numbers[count]);
     }
 }
