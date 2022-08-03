c3a5af75963cdb32dc2f05c04e74c1ccf8158fe6
Parampatel07 <90458806+Parampatel07@users.noreply.github.com>
2022-08-03 20:21:44 +0530

    functions

---
+++ function1.c
@@ -0,0 +1,18 @@
+//example of function 
+// 1]without argument without return value
+#include<stdio.h>
+// datatype function_name(arugment/input)
+// {
+
+// }
+void printLine()
+{
+    printf("***********************************\n");
+}
+void main()
+{
+    //calling the function
+    printLine();
+    printf("Param patel\n");
+    printLine();
+}
---
+++ function2.c
@@ -0,0 +1,14 @@
+//write a programe to find area of triagnle using function where height is 20 and base is 10
+#include<stdio.h>
+void Getarea()
+{
+    int height,base,area;
+    height=20;
+    base=10;
+    area=height*base/2;
+    printf("area of triagnle is %d",area);
+}
+void main()
+{
+    Getarea();
+}
